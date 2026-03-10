ad6d7b81f1815a94e0ecd0ea1e093e461b4c33a9
unknown <contact.crosswears@gmail.com>
2026-03-10 22:00:22 +0200

    First commit

---
+++ src/main/java/com/example/Delivro/DelivroApplication.java
@@ -0,0 +1,13 @@
+package com.example.Delivro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelivroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelivroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Delivro/DelivroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Delivro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelivroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
